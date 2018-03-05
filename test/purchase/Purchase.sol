f0c6b8eac0389e1c1dd2dd882e7e1ba10174d93d
jimmy <sergii.pryhodko@gmail.com>
2018-03-05 22:51:14 +0200

    purchase contractis added

---
+++ test/purchase/Purchase.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.0;
+
+contract Purchase {
+    uint public value;
+    address public seller;
+    address public buyer;
+    enum State {Create, Locked, Inactive}
+    State public state;
+    function Purchase() public payable{
+        seller = msg.sender;
+        value = msg.value/2;
+        require((2*value) == msg.value);
+    }
+
+    modifier condition(bool _condition){
+        require(_condition); _;
+    }
+
+    modifier onlyBuyer(){
+        require(msg.sender == buyer); _;
+    }
+
+    modifier onlySeller(){
+        require(msg.sender == seller); _;
+    }
+
+    modifier inState(State state){
+        require(state == _state); _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+
+    function abort() public
+    onlySeller
+    inState(State.Created){
+        emit Aborted();
+        state = State.Inactive;
+        seller.transfer(this.balance);
+    }
+
+    function confirmPurchase() public
+    inState(State.Created)
+    condition(msg.value == (2*value))
+    payable{
+        emit PurchaseConfirmed();
+        buyer = msg.sender;
+        state = State.Locked;
+    }
+
+    function confirmReceived() public
+    onlyBuyer
+    inState(State.Locked){
+        emit ItemReceived();
+        state = State.Inactive;
+        buyer.transfer(value);
+        seller.transfer(this.balance);
+    }
+}
